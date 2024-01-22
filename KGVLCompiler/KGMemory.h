22918b5549082d231eca35955ec2312fbc10c096
amogus <86293484+WOODEN-CHEST@users.noreply.github.com>
2024-01-22 11:52:17 +0200

    Delete KGVLCompiler directory
    I ain't up to the challenge of making this in C

--- KGVLCompiler/KGMemory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stddef.h>
-
-// Functions.
-void* Memory_SafeMalloc(size_t size);
-
-void* Memory_SafeRealloc(void* pointer, size_t newSize);
-
-void* Memory_Free(void* pointer);
